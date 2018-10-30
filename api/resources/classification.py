1b510e369cd595246f821e5c4bfbdd0b304fb2fd
raphaelmeudec <raphael.meudec@gmail.com>
2018-10-30 18:30:42 +0100

    add basic classification task

--- api/resources/classification.py
+++ api/resources/classification.py
@@ -4,10 +4,26 @@ Define the REST verbs relative to the classification
 from flask_restful import Resource
 
 from keras.applications import ResNet50
+from keras.applications.resnet50 import preprocess_input
+from keras.preprocessing import image
+import numpy as np
+import tensorflow as tf
 
+global model
+model = ResNet50()
+global graph
+graph = tf.get_default_graph()
 
 class ClassificationResource(Resource):
     @staticmethod
     def get():
-        model = ResNet50()
-        return 'model loaded!'
+        img = image.load_img('assets/cat.jpg', target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        with graph.as_default():
+            results = model.predict(x)
+            print(results, flush=True)
+
+        return 'prediction done!'
